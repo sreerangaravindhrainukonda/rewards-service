392e996b81afb9e7e472162ade99b131574862e0
Sreeranga <inukonda.ravindra@gmail.com>
2024-02-12 10:56:21 -0800

    Test cases added

--- src/main/java/com/assignment/microservice/rawardservice/model/Customer.java
+++ src/main/java/com/assignment/microservice/rawardservice/model/Customer.java
@@ -3,12 +3,10 @@ package com.assignment.microservice.rawardservice.model;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Transient;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
@@ -19,7 +17,7 @@ public class Customer {
     @JsonIgnore
     private Integer id;
     private String name;
-    @OneToMany(mappedBy = "customer", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
     private Set<CustomerTransaction> transactions;
     @JsonInclude
     @Transient
@@ -27,53 +25,54 @@ public class Customer {
     @JsonInclude
     @Transient
     private Double totalPurchases;
-
+    
     public Customer() {
         super();
     }
-
+    
     public Customer(Integer id, String name) {
         super();
         this.id = id;
         this.name = name;
     }
-
+    
     public Integer getId() {
         return id;
     }
-
+    
     public void setId(Integer id) {
         this.id = id;
     }
-
+    
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
-
+    
     public Set<CustomerTransaction> getTransactions() {
         return transactions;
     }
-
+    
     public void setTransactions(Set<CustomerTransaction> transactions) {
         this.transactions = transactions;
     }
-
+    
     public Long getRewardPoints() {
-        if (transactions == null || transactions.isEmpty()) return 0l;
-
-        return transactions.stream().map(x -> x.getPoints().intValue()).reduce(0, (a, b) -> a + b).longValue();
+        return rewardPoints;
     }
-
+    
+    public void setRewardPoints(Long rewardPoints) {
+        this.rewardPoints = rewardPoints;
+    }
+    
     public Double getTotalPurchases() {
-        if (transactions == null || transactions.isEmpty()) return 0d;
-
-        return transactions.stream().map(x -> x.getTotal().doubleValue()).reduce(0d, (a, b) -> a + b).doubleValue();
+        return totalPurchases;
     }
-
-
+    
+    public void setTotalPurchases(Double totalPurchases) {
+        this.totalPurchases = totalPurchases;
+    }
 }
-
